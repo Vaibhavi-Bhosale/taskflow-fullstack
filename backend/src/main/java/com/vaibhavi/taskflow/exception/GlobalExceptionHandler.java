8dbce7f01e01a855cd744405a03210e594dcefd6
Vaibhavi-Bhosale <vaibhavibhosale.official1@gmail.com>
2026-06-29 19:54:20 +0530

    Implement DTO based error handling

--- backend/src/main/java/com/vaibhavi/taskflow/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/vaibhavi/taskflow/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.vaibhavi.taskflow.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import com.vaibhavi.taskflow.dto.ErrorResponse;
+
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,12 +14,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException ex)
+
+
+    public ResponseEntity<ErrorResponse> handleTaskNotFoundException(TaskNotFoundException ex)
     {
+
+          ErrorResponse error = new ErrorResponse( " ooh bhaii Task not found", 404) ;
+
 
           return  ResponseEntity
                   .status(HttpStatus.NOT_FOUND)
-                  .body(ex.getMessage());
+                  .body( error);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
